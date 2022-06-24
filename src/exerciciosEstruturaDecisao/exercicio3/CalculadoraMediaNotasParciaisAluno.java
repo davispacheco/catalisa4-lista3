99cd960c2eb79c788f70399fc3fd727433fe2e9c
davispacheco <davipacheco1996@gmail.com>
2022-06-24 16:27:44 -0300

    Adicionar classe CalculadoraReajusteSalarial, com a resolução do exercício 4.

--- src/exerciciosEstruturaDecisao/exercicio3/CalculadoraMediaNotasParciaisAluno.java
+++ src/exerciciosEstruturaDecisao/exercicio3/CalculadoraMediaNotasParciaisAluno.java
@@ -1,10 +1,26 @@
 package exerciciosEstruturaDecisao.exercicio3;
 
 import java.util.Scanner;
+
 public class CalculadoraMediaNotasParciaisAluno {
 
     public static void main(String[] args) {
 
-        
+        Scanner input = new Scanner(System.in);
+        double[] notasParciais = new double[2];
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Digite sua " + (i + 1) + "ª nota:");
+            notasParciais[i] = input.nextDouble();
+        }
+        input.close();
+        double media = (notasParciais[0] + notasParciais[1]) / notasParciais.length;
+        System.out.printf("Sua média é: %.1f", media);
+        if (media == 10) {
+            System.out.println("\nAprovado com distinção!");
+        } else if (media >= 7) {
+            System.out.println("\nAprovado.");
+        } else {
+            System.out.println("\nReprovado.");
+        }
     }
 }
