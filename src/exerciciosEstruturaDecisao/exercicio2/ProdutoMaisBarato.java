441c3b833321bcc24795482d47a915769ef24fb8
davispacheco <davipacheco1996@gmail.com>
2022-06-24 16:00:58 -0300

    Adicionar classe CalculadoraNotasParciaisAluno, com a resoluçao do exercício 3.

--- src/exerciciosEstruturaDecisao/exercicio2/ProdutoMaisBarato.java
+++ src/exerciciosEstruturaDecisao/exercicio2/ProdutoMaisBarato.java
@@ -1,2 +1,27 @@
-package exerciciosEstruturaDecisao.exercicio2;public class ProdutoMaisBarato {
+package exerciciosEstruturaDecisao.exercicio2;
+
+import java.util.Scanner;
+public class ProdutoMaisBarato {
+
+    public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+double[] precosProdutos = new double[3];
+for (int i = 0; i < 3; i ++) {
+    System.out.println("Informe o preço do " + (i+1) + "º produto:");
+    precosProdutos[i] = input.nextDouble();
+}
+double resultado;
+if(precosProdutos[0] < precosProdutos[1] && precosProdutos[0] < precosProdutos[2]) {
+resultado = precosProdutos[0];
+}
+else if(precosProdutos[1] < precosProdutos[0] && precosProdutos[1] < precosProdutos[2]) {
+    resultado = precosProdutos[1];
+}
+else {
+    resultado = precosProdutos[2];
+}
+        System.out.printf("O produto que você deverá comprar é o que custa R$%.2f", resultado);
+System.out.printf(", pois está mais barato.");
+
+    }
 }
---
+++ src/exerciciosEstruturaDecisao/exercicio3/CalculadoraMediaNotasParciaisAluno.java
@@ -0,0 +1,10 @@
+package exerciciosEstruturaDecisao.exercicio3;
+
+import java.util.Scanner;
+public class CalculadoraMediaNotasParciaisAluno {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
