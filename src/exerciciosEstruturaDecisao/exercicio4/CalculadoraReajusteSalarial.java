e6f548547c90c03dab018ec834967a0c75221a66
davispacheco <davipacheco1996@gmail.com>
2022-06-24 16:29:04 -0300

    Organizar identação das linhas do código referente à classe CalculadoraReajusteSalarial.

--- src/exerciciosEstruturaDecisao/exercicio4/CalculadoraReajusteSalarial.java
+++ src/exerciciosEstruturaDecisao/exercicio4/CalculadoraReajusteSalarial.java
@@ -1,6 +1,7 @@
 package exerciciosEstruturaDecisao.exercicio4;
 
 import java.util.Scanner;
+
 public class CalculadoraReajusteSalarial {
 
     public static void main(String[] args) {
@@ -9,20 +10,17 @@ public class CalculadoraReajusteSalarial {
         System.out.println("Informe qual o seu salário:");
         double salarioAntigo = input.nextDouble();
         input.close();
-double reajuste;
-if (salarioAntigo < 280) {
-    reajuste = salarioAntigo * 0.2;
-}
-else if (salarioAntigo >= 280 && salarioAntigo < 700) {
-    reajuste = salarioAntigo * 0.15;
-}
-else if (salarioAntigo >= 700 && salarioAntigo < 1500) {
-    reajuste = salarioAntigo * 0.1;
-}
-else {
-    reajuste = salarioAntigo * 0.05;
-}
-double salarioNovo = salarioAntigo + reajuste;
+        double reajuste;
+        if (salarioAntigo < 280) {
+            reajuste = salarioAntigo * 0.2;
+        } else if (salarioAntigo >= 280 && salarioAntigo < 700) {
+            reajuste = salarioAntigo * 0.15;
+        } else if (salarioAntigo >= 700 && salarioAntigo < 1500) {
+            reajuste = salarioAntigo * 0.1;
+        } else {
+            reajuste = salarioAntigo * 0.05;
+        }
+        double salarioNovo = salarioAntigo + reajuste;
         System.out.printf("Salário antigo: R$%.2f", salarioAntigo);
         System.out.printf("\nValor adicional de reajuste: R$%.2f", reajuste);
         System.out.printf("\nNovo salário: R$%.2f", salarioNovo);
