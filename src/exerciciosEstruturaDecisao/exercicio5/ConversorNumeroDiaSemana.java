2557d49b35b3b35495d86ad4843894117ae10a5a
davispacheco <davipacheco1996@gmail.com>
2022-06-26 13:14:40 -0300

    Adicionar classe ConversorNumeroDiaSemana, com a resolução do exercício 5.

---
+++ src/exerciciosEstruturaDecisao/exercicio5/ConversorNumeroDiaSemana.java
@@ -0,0 +1,38 @@
+package exerciciosEstruturaDecisao.exercicio5;
+
+import java.util.Scanner;
+
+public class ConversorNumeroDiaSemana {
+
+    public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
+        System.out.println("Digite sua opção:");
+        int opcao = input.nextInt();
+        switch (opcao) {
+            case 1:
+                System.out.println("Dia da semana: Domingo");
+                break;
+            case 2:
+                System.out.println("Dia da semana: Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Dia da semana: Terça-feira");
+                break;
+            case 4:
+                System.out.println("Dia da semana: Quarta-feira");
+break;
+            case 5:
+                System.out.println("Dia da semana: Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Dia da semana: Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Dia da semana: Sábado");
+                break;
+            default:
+                System.out.println("Opção inválida, tente novamente!");
+        }
+    }
+}
