08ecba909c2a57f15d3cecf5324ed4dd83a24a51
davispacheco <davipacheco1996@gmail.com>
2022-06-26 14:59:36 -0300

    Adicionar classe CalculadoraMediaNumerosIndeterminados, com a resolução do exercício 2.

---
+++ src/exerciciosEstruturaRepeticao/exercicio2/CalculadoraMediaNumerosIndeterminados.java
@@ -0,0 +1,30 @@
+package exerciciosEstruturaRepeticao.exercicio2;
+
+import java.util.Scanner;
+
+public class CalculadoraMediaNumerosIndeterminados {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Informe com quantos números deseja fazer a média:");
+        int quantidadeNumeros = input.nextInt();
+        if (quantidadeNumeros <= 0) {
+            System.out.println("Número inválido para quantidade, escolha outro.");
+        } else {
+            int soma = 0;
+            for (int i = 1; i <= quantidadeNumeros; i++) {
+                System.out.println("Digite o " + i + "º número:");
+                int numero = input.nextInt();
+                if (numero <= 0) {
+                    System.out.println("Número incorreto para realizar a média.");
+                    break;
+                } else {
+                    soma += numero;
+                }
+                int media = soma / quantidadeNumeros;
+                System.out.println("A média dos números informados é: " + media);
+            }
+            input.close();
+        }
+    }
+}
