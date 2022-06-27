83310f2ff169cae58b7d632e2c1f87ba150339f1
davispacheco <davipacheco1996@gmail.com>
2022-06-26 21:17:06 -0300

    Adicionar classe IncrementadorNumero, com a resolução do exercício 5.

---
+++ src/exerciciosEstruturaRepeticao/exercicio5/IncrementadorNumero.java
@@ -0,0 +1,11 @@
+package exerciciosEstruturaRepeticao.exercicio5;
+
+public class IncrementadorNumero {
+    public static void main(String[] args) {
+int valor = 0;
+while (valor < 100000) {
+    valor += 1000;
+    System.out.println(valor);
+}
+    }
+}
