e917553cebb1f099ff0064d57a787992d2ba5faa
davispacheco <davipacheco1996@gmail.com>
2022-06-27 00:04:58 -0300

    Adicionar classe CalculadoraSomaFixaNumerosImpares, com a resolução do exercício 8.

---
+++ src/exerciciosEstruturaRepeticao/exercicio8/CalculadoraSomaFixaNumerosImpares.java
@@ -0,0 +1,15 @@
+package exerciciosEstruturaRepeticao.exercicio8;
+
+public class CalculadoraSomaFixaNumerosImpares {
+    public static void main(String[] args) {
+        int contador = 1;
+int soma = 0;
+while (contador < 100) {
+    if (contador % 2 > 0) {
+        soma += contador;
+    }
+    contador ++;
+        }
+        System.out.println("A soma dos 50 primeiros números ímpares é: "+soma);
+}
+}
